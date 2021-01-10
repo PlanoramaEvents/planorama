0da9943572fa45897925fd3d3a9c9a29c588e52f
henry@grenadine.co <henry@grenadine.co>
2021-01-10 16:37:38 -0500

    WIP

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+const erb = require('./loaders/erb')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('erb', erb)
 module.exports = environment
