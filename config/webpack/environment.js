1c418abb7e16dc8f24a46af658be19ceea656b75
Gail Terman <gterman@gmail.com>
2021-01-23 21:38:55 -0500

    fix webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,9 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-const erb = require('./loaders/erb')
+const { webpackConfig, merge } = require('@rails/webpacker')
+const vueConfig = require('./loaders/vue')
+const customConfig = {
+    resolve: {
+        extensions: ['.css', '.scss']
+    }
+}
 
-const webpack = require('webpack')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.loaders.prepend('erb', erb)
-module.exports = environment
+module.exports = merge(vueConfig, webpackConfig, customConfig)
--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,6 +1,24 @@
+const { VueLoaderPlugin } = require('vue-loader')
 module.exports = {
-  test: /\.vue(\.erb)?$/,
-  use: [{
-    loader: 'vue-loader'
-  }]
+  module: {
+    rules: [
+      {
+        test: /\.vue/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.vue(\.erb)?$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
 }
