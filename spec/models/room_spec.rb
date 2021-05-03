7bff8e687b4112ff37c95c3ff3f51c2e82550535
Leane Verhulst <lverhulst@gmail.com>
2021-05-02 22:26:17 -0500

    Worked on the factorybot tests.

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Room, "#exists"  do
-    context "does this even work" do
+RSpec.describe Room, "#factories"  do
+    context "room factory" do
         it "creates a room" do
             room = create(:room)
+            expect(room.name).to include("my room")
+            expect(room.purpose).to be_nil
+            expect(room.comment).to be_nil
+            expect(room.sort_order).to eq 0
+            expect(room.capacity).to be_nil
+        end
+    end
+
+    context "room_with_detail factory" do
+        it "creates a room with details" do
+            room = create(:room_with_detail)
+            expect(room.purpose).to_not be_empty
+            expect(room.comment).to_not be_empty
+            expect(room.sort_order).to be_an(Integer)
+            expect(room.capacity).to be_an(Integer)
         end
     end
 end
