66268f89c17068595f323624ff0906086b344060
Henry <henry.balen@chicon.org>
2021-05-09 17:26:45 -0400

    make navbar bootstarp vue

--- app/javascript/packs/navbar_vue.js
+++ app/javascript/packs/navbar_vue.js
@@ -1,13 +1,14 @@
 /* eslint no-console: 0 */
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { NavbarPlugin } from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
+Vue.use(NavbarPlugin)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#top-navbar',
     data: () => {
+      console.debug('navbar ', location.pathname)
       return {
         isActive: true,
         currentLocation: location.pathname
