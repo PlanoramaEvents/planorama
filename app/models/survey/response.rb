a75a0d86224f0d0500194a272d36b5a37202d7c0
Henry <henry.balen@chicon.org>
2021-06-27 16:18:08 -0400

    fix typo in relatonship

--- app/models/survey/response.rb
+++ app/models/survey/response.rb
@@ -1,5 +1,4 @@
 class Survey::Response < ApplicationRecord
   belongs_to :survey_question
-
-  belongs_to :survey_subnission, foreign_key: 'survey_submission_id', class_name: 'Survey::Submission'
+  belongs_to :survey_submission, foreign_key: 'survey_submission_id', class_name: 'Survey::Submission'
 end
