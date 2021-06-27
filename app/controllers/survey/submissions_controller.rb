9a0e21347ea21335d0e423ae843788c2d5c16d59
Henry <henry.balen@chicon.org>
2021-06-27 17:15:02 -0400

    WIP on submission save

--- app/controllers/survey/submissions_controller.rb
+++ app/controllers/survey/submissions_controller.rb
@@ -2,14 +2,40 @@ class Survey::SubmissionsController < ResourceController
   MODEL_CLASS = 'Survey::Submission'.freeze
   POLICY_CLASS = 'SubmissionsPolicy'.freeze
 
-  # TODO: create and update
-
-  # TODO: need the survey id etc as a parameter to the collection/index method
   def belong_to_class
     Survey
   end
 
   def belongs_to_relationship
     'survey_submissions'
+  end
+
+  def before_save
+    # Make sure the submission is assigned to the current person
+    @object.person_id = current_person.id
+  end
+
+  def includes
+    [
+      :survey_responses
+    ]
+  end
+
+
+  def allowed_params
+    %i[
+      lock_version
+      person_id
+      survey_id
+      survey_responses
+    ] << [
+      survey_responses_attributes: %i[
+        id
+        _destroy
+        survey_submission_id
+        survey_question_id
+        response
+      ]
+    ]
   end
 end
