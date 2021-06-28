595fac7eaf36cf93313d26edbfb861a94d506ad0
Henry <henry.balen@chicon.org>
2021-06-27 20:56:44 -0400

    code tidying up

--- app/models/survey/response.rb
+++ app/models/survey/response.rb
@@ -1,4 +1,10 @@
 class Survey::Response < ApplicationRecord
-  belongs_to :survey_question, class_name: 'Survey::Question', foreign_key: 'survey_question_id'
-  belongs_to :survey_submission, foreign_key: 'survey_submission_id', class_name: 'Survey::Submission'
+  belongs_to :survey_question,
+             class_name: 'Survey::Question',
+             foreign_key: 'survey_question_id'
+
+  belongs_to :survey_submission,
+             class_name: 'Survey::Submission',
+             foreign_key: 'survey_submission_id',
+             inverse_of: :survey_responses
 end
--- app/models/survey/submission.rb
+++ app/models/survey/submission.rb
@@ -2,6 +2,10 @@ class Survey::Submission < ApplicationRecord
   belongs_to :survey
   belongs_to :person
 
-  has_many :survey_responses, foreign_key: 'survey_submission_id', class_name: 'Survey::Response', dependent: :destroy
+  has_many :survey_responses,
+           class_name: 'Survey::Response',
+           foreign_key: 'survey_submission_id',
+           inverse_of: :survey_submission,
+           dependent: :destroy
   accepts_nested_attributes_for :survey_responses, allow_destroy: true
 end
