29a3b7c7dcbe96c2c9d397ab1f3b93c594dcfc89
Henry <henry.balen@chicon.org>
2021-06-28 07:21:12 -0400

    convert response to searchable text on save

--- app/models/survey/response.rb
+++ app/models/survey/response.rb
@@ -7,4 +7,28 @@ class Survey::Response < ApplicationRecord
              class_name: 'Survey::Submission',
              foreign_key: 'survey_submission_id',
              inverse_of: :survey_responses
+
+  before_save :set_response_text
+
+  #
+  # Extract the values from all the entries and save a plain text
+  # version that can be used for searchin the responses in a "report"
+  #
+  def set_response_text
+    flattened_response = flatten_response(response)
+    self.response_as_text = flattened_response.values.join(' ').strip
+  end
+
+  #
+  def flatten_response(hash)
+    hash.each_with_object({}) do |(k, v), h|
+      if v.is_a? Hash
+        flatten_response(v).map do |h_k, h_v|
+        h["#{k}.#{h_k}".to_sym] = h_v
+        end
+      else
+        h[k] = v
+      end
+    end
+  end
 end
