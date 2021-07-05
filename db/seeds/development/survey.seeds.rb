673b354ab50aa2a8232443476c6385583ee40d3d
annA <597368+arbradley@users.noreply.github.com>
2021-07-05 01:35:36 -0400

    trying to make my own surveys with  'real' questions
    trying this thing.  with my own questions.

--- db/seeds/development/survey.seeds.rb
+++ db/seeds/development/survey.seeds.rb
@@ -7,6 +7,83 @@ Survey.destroy_all
 # Survey::Question.delete_all
 # Survey::Answer.delete_all
 
+#----------------
+
+
+ # name = Faker::Books::CultureSeries.book
+
+  creator = Person.all.sample.id
+  survey = Survey.create(
+    name: "Anna's First Survey",
+    thank_you: Faker::Lorem.sentence,
+    #alias: name.gsub(/\s+/, '_'),
+    submit_string: 'submit',
+    header_image: Faker::LoremFlickr.image(size: "1000x100"),
+    use_captcha: Faker::Boolean.boolean(true_ratio: 0.8),
+    public: Faker::Boolean.boolean(true_ratio: 0.8),
+    authenticate: Faker::Boolean.boolean(true_ratio: 0.8),
+    transition_accept_status: false,
+    transition_decline_status: false,
+    welcome: Faker::Lorem.sentence,
+    description: "For my first survey, i bet you are all wondering why i brought you here today.  Mawwige, mawwige is what brights us together today.  I am a very model of a modern major general.  Something something something vegetable and mineral.",
+    declined_msg: Faker::Lorem.sentence,
+    authenticate_msg: Faker::Lorem.sentence,
+    anonymous: false,
+    created_by_id: creator,
+    updated_by_id: creator
+  )
+
+  #page_title = Faker::Books::CultureSeries.civ
+  survey_page = Survey::Page.create(
+    title: "Page the very first",
+    survey_id: survey.id
+  )
+survey_page2 = Survey::Page.create(
+    title: "Page the tottaly second",
+    survey_id: survey.id
+  )
+
+
+   # question_type= [:textfield, :textbox, :singlechoice, :multiplechoice, :hr, :dropdown, :email, :address, :socialmedia].sample;
+   
+
+    survey_question = Survey::Question.create(
+      question: "What is your name?",
+      question_type: :textfield,
+      text_size: Faker::Number.between(from: 11, to: 16),
+      horizontal: Faker::Boolean.boolean,
+      survey_page_id: survey_page.id
+    )
+    survey_question2 = Survey::Question.create(
+          question: "What is your quest?",
+          question_type: :textfield,
+          text_size: Faker::Number.between(from: 11, to: 16),
+          horizontal: Faker::Boolean.boolean,
+          survey_page_id: survey_page.id
+        )
+    survey_question3 = Survey::Question.create(
+          question: "What is the air speed velocity of an unladen swallow?",
+          question_type: :singlechoice,
+          text_size: Faker::Number.between(from: 11, to: 16),
+          horizontal: Faker::Boolean.boolean,
+          survey_page_id: survey_page2.id
+        )
+    Survey::Answer.create(
+              survey_question_id: survey_question3.id,
+              answer: "African"
+            )
+    Survey::Answer.create(
+              survey_question_id: survey_question3.id,
+              answer: "European"
+            )
+    Survey::Answer.create(
+              survey_question_id: survey_question3.id,
+              answer: "I don't know"
+            )
+   
+
+#----------------
+
 10.times.each do |i|
   name = Faker::Books::CultureSeries.book
 
