4d3b8ecbbbb742f8713c6def0413970883dc7051
Henry <henry.balen@chicon.org>
2021-08-11 15:12:54 -0400

    changed to use env variables for secret and mailer

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,23 @@
+Rails.application.config.action_mailer.tap do |action_mailer|
+  return if Rails.env.development?
+
+  if Rails.env.test?
+    action_mailer.default_url_options = {
+      host: "localhost"
+    }
+  else
+    action_mailer.delivery_method = :smtp
+    action_mailer.tap do |action_mailer|
+      action_mailer.raise_delivery_errors = true
+      action_mailer.smtp_settings = {
+        address:              ENV["SMTP_SERVER"],
+        port:                 ENV["SMTP_PORT"],
+        user_name:            ENV["SMTP_USER_NAME"],
+        password:             ENV["SMTP_PASSWORD"],
+        authentication:       "plain",
+        enable_starttls_auto: true,
+        ssl:                  ENV["SMTP_PORT"].to_i == 465
+      }
+    end
+  end
+end
