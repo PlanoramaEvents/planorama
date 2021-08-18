22ffe1370300585f7f68d42083f0494895679060
Henry <henry.balen@chicon.org>
2021-08-18 12:36:35 -0400

    add in consistency check for next_page_id

--- app/serializers/survey/answer_serializer.rb
+++ app/serializers/survey/answer_serializer.rb
@@ -1,5 +1,11 @@
 class Survey::AnswerSerializer < ActiveModel::Serializer
   attributes :id, :answer, :default, :created_at,
              :updated_at, :lock_version, :survey_question_id,
-             :sort_order, :next_page_id, :fuuid, :other
+             :sort_order, :fuuid, :other
+
+  attribute :next_page_id do
+    # consisteny check
+    # next_page is either null, -1 or a valid survey_page_id
+    object.next_page_id
+  end
 end
