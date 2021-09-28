8e5e875d44703850594233b3ec15113c9393b85c
Henry <henry.balen@chicon.org>
2021-09-28 09:00:44 -0400

    take out test button

--- app/javascript/people/people-screen.vue
+++ app/javascript/people/people-screen.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <button type="button" class="btn btn-secondary btn-sm" @click="getPeople">
-      GET PEOPLE
-    </button>
-
     <people-table></people-table>
     <people-sidebar></people-sidebar>
   </div>
@@ -20,15 +16,6 @@ export default {
   components: {
     PeopleTable,
   //   PeopleSidebar,
-  },
-  methods: {
-    getPeople() {
-      // console.debug('******** GET PEOPLE CALLED: ', this.$store)
-      this.$store.dispatch('jv/get', 'people').then((data) => {
-        console.log('****', data._jv.json.meta)
-        // console.log(data['0beae1ed-fddc-409e-a702-52d5da654519'].bio)
-      })
-    }
   }
 }
 </script>
