789c471bb51e656b2af404476a8ccd70d912bde9
Henry <henry.balen@chicon.org>
2021-10-14 15:44:12 -0400

    people sidebar WIP

--- app/javascript/people/people_sidebar.vue
+++ app/javascript/people/people_sidebar.vue
@@ -1,35 +1,41 @@
 <template>
-  <sidebar-vue
-    model="person"
-    v-slot:content
-  >
-    <b-tabs content-class="mt-3">
-      <b-tab title="Details">
-        <bio></bio>
-        <pre>{{ selected }}</pre>
-      </b-tab>
-      <b-tab title="Bio">
-        <bio></bio>
-        <pre>{{ selected.bio }}</pre>
-      </b-tab>
-      <b-tab title="Tags">
-        <p>TODO: Tags</p>
-      </b-tab>
-    </b-tabs>
-    <!-- <pre>{{ selected }}</pre> -->
+  <sidebar-vue model="person">
+    <template v-slot:header>
+      <h1>
+        {{selected.published_name}}
+      </h1>
+    </template>
+
+    <template v-slot:content>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Details">
+          <pre>{{ selected }}</pre>
+        </b-tab>
+        <b-tab title="Emails">
+          <pre>{{ selected.email_addresses }}</pre>
+        </b-tab>
+        <b-tab title="Bio">
+          <pre>{{ selected.bio }}</pre>
+        </b-tab>
+        <b-tab title="Roles">
+          <pre>{{ selected.person_roles }}</pre>
+        </b-tab>
+        <b-tab title="Tags">
+          <p>TODO: Tags</p>
+        </b-tab>
+      </b-tabs>
+    </template>
   </sidebar-vue>
 </template>
 
 <script>
 import SidebarVue from '../components/sidebar_vue';
 import modelMixin from '../store/model.mixin';
-//import Bio from './bio';
 
 export default {
   name: 'PeopleSidebar',
   components: {
-    SidebarVue,
- //   Bio
+    SidebarVue
   },
   mixins: [
     modelMixin
