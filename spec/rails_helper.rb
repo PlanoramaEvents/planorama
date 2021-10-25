e7b763fc2064ff8546481cc1df4786f246c6c55c
Henry <henry.balen@chicon.org>
2021-10-25 11:56:59 -0400

    let rake load seed data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,8 +66,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include RequestSpecHelper, type: :request
-
-  config.before(:suite) do
-    Rails.application.load_seed # loading seeds
-  end
 end
