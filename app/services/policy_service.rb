e564d725b9cd7ee9ae88fa84dadf14ec465e2441
Henry <henry.balen@chicon.org>
2021-10-31 12:54:08 -0400

    extend rbac info to all controllers

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -1,28 +1,42 @@
 # We want to get a list of the policies for a user
 module PolicyService
   def self.policies_for(person:)
-    classes = [:Submissions, :Surveys, :People, :Agreements]
+    Rails.application.eager_load!
+
+    classes = ResourceController.descendants.collect{|c| name_for_class(clazz: c)}
 
     permissions = { }
 
     classes.each do |clazz|
-      entity = clazz.to_s.singularize.downcase
+      entity = clazz.to_s.singularize.snakecase
       permissions[entity] = {}
-      policy =  Pundit.policy(person, clazz)
+      policy = Pundit.policy(person, clazz.to_sym)
+      # if nill use PlannerPolicy
+      policy ||= Pundit.policy(person, :Planner)
       policy.public_methods(false).sort.each do |m|
         result = policy.send m
         op = m.to_s.gsub(/\?$/, '')
         permissions[entity][op] = result
       end
 
-      super_policy = policy.class.superclass.new(person, clazz)
-      super_policy.public_methods(false).sort.each do |m|
-        result = policy.send m
-        op = m.to_s.gsub(/\?$/, '')
-        permissions[entity][op] = result
+      if policy.class != PlannerPolicy
+        super_policy = policy.class.superclass.new(person, clazz)
+        super_policy.public_methods(false).sort.each do |m|
+          result = policy.send m
+          op = m.to_s.gsub(/\?$/, '')
+          permissions[entity][op] = result
+        end
       end
     end
 
     permissions
+  end
+
+  def self.name_for_class(clazz:)
+    if defined?(self.class::MODEL_CLASS)
+      clazz::MODEL_CLASS #.constantize
+    else
+      clazz.name.sub('Controller', '') #.singularize #.constantize
+    end
   end
 end
