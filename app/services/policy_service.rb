4cdb5a91e1ca0877275bc0fae223e819e759103f
Henry <henry.balen@chicon.org>
2021-10-31 09:39:19 -0400

    singularize in policy rbac get the methods from the parent as well

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -6,12 +6,19 @@ module PolicyService
     permissions = { }
 
     classes.each do |clazz|
-      permissions["#{clazz}"] = {}
+      permissions["#{clazz.to_s.singularize}"] = {}
       policy =  Pundit.policy(person, clazz)
       policy.public_methods(false).sort.each do |m|
         result = policy.send m
         op = m.to_s.gsub(/\?$/, '')
-        permissions["#{clazz}"][op] = result
+        permissions["#{clazz.to_s.singularize}"][op] = result
+      end
+
+      super_policy = policy.class.superclass.new(person, clazz)
+      super_policy.public_methods(false).sort.each do |m|
+        result = policy.send m
+        op = m.to_s.gsub(/\?$/, '')
+        permissions["#{clazz.to_s.singularize}"][op] = result
       end
     end
 
