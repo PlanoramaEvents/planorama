3deec169ce4f3a0a8671122298e117accf1cafdb
Henry <henry.balen@chicon.org>
2021-10-31 14:31:29 -0400

    small code cleanup

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -1,7 +1,7 @@
 # We want to get a list of the policies for a user
 module PolicyService
   def self.policies_for(person:)
-    Rails.application.eager_load!
+    Rails.application.eager_load! # ensure that the controller classes are loaded
 
     classes = ResourceController.descendants.collect{|c| name_for_class(clazz: c)}
 
@@ -11,22 +11,23 @@ module PolicyService
       entity = clazz.to_s.singularize.snakecase
       permissions[entity] = {}
       policy = Pundit.policy(person, clazz.to_sym)
-      # if nill use PlannerPolicy
-      policy ||= Pundit.policy(person, :Planner)
-      policy.public_methods(false).sort.each do |m|
-        result = policy.send m
-        op = m.to_s.gsub(/\?$/, '')
-        permissions[entity][op] = result
-      end
+      policy ||= Pundit.policy(person, :Planner) # if nill use PlannerPolicy
+      build_policy_entries(permissions: permissions[entity], policy: policy)
 
-      if policy.class != PlannerPolicy
-        super_policy = policy.class.superclass.new(person, clazz)
-        super_policy.public_methods(false).sort.each do |m|
-          result = policy.send m
-          op = m.to_s.gsub(/\?$/, '')
-          permissions[entity][op] = result
-        end
-      end
+      next unless policy.class != PlannerPolicy
+
+      super_policy = policy.class.superclass.new(person, clazz)
+      build_policy_entries(permissions: permissions[entity], policy: super_policy)
+    end
+
+    permissions
+  end
+
+  def self.build_policy_entries(permissions:, policy:)
+    policy.public_methods(false).sort.each do |m|
+      result = policy.send m
+      op = m.to_s.gsub(/\?$/, '')
+      permissions[op] = result
     end
 
     permissions
