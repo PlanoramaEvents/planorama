adf882652b888382c0ea0b6fc94d2059d1aa9fcc
Henry <henry.balen@chicon.org>
2021-10-31 11:20:28 -0400

    downcase rbac entities

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -6,19 +6,20 @@ module PolicyService
     permissions = { }
 
     classes.each do |clazz|
-      permissions["#{clazz.to_s.singularize}"] = {}
+      entity = clazz.to_s.singularize.downcase
+      permissions[entity] = {}
       policy =  Pundit.policy(person, clazz)
       policy.public_methods(false).sort.each do |m|
         result = policy.send m
         op = m.to_s.gsub(/\?$/, '')
-        permissions["#{clazz.to_s.singularize}"][op] = result
+        permissions[entity][op] = result
       end
 
       super_policy = policy.class.superclass.new(person, clazz)
       super_policy.public_methods(false).sort.each do |m|
         result = policy.send m
         op = m.to_s.gsub(/\?$/, '')
-        permissions["#{clazz.to_s.singularize}"][op] = result
+        permissions[entity][op] = result
       end
     end
 
