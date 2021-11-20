4128f54b75181edfb8599ffa3d54ad09a00c6ef6
Henry <henry.balen@chicon.org>
2021-11-20 13:13:45 -0500

    do not destroy data

--- db/seeds/venue.seeds.rb
+++ db/seeds/venue.seeds.rb
@@ -1,17 +1,17 @@
-Venue.destroy_all
+if Person.count == 0
+  3.times.each do |i|
+      v = Venue.create(
+          name: Faker::Space.unique.planet,
+          sort_order: Faker::Number.unique.number(digits: 2)
+      )
+      5.times.each do |j|
+          Room.create(
+              name: Faker::Mountain.unique.name,
+              venue: v,
+              sort_order: Faker::Number.unique.number(digits: 3)
+          )
+      end
+  end
 
-3.times.each do |i|
-    v = Venue.create(
-        name: Faker::Space.unique.planet,
-        sort_order: Faker::Number.unique.number(digits: 2)
-    )
-    5.times.each do |j|
-        Room.create(
-            name: Faker::Mountain.unique.name,
-            venue: v,
-            sort_order: Faker::Number.unique.number(digits: 3)
-        )
-    end
+  p "Created #{Venue.count} venues with #{Room.count} rooms."
 end
-
-p "Created #{Venue.count} venues with #{Room.count} rooms."
