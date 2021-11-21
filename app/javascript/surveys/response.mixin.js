82f2baa84f1a9be4d21a85af80b78a32949bfe8b
Gail Terman <gterman@gmail.com>
2021-11-21 12:31:15 -0500

    nearly working responses

--- app/javascript/surveys/response.mixin.js
+++ app/javascript/surveys/response.mixin.js
@@ -1,23 +1,28 @@
 import { utils } from "jsonapi-vuex"
 import { submissionModel, questionModel, responseModel as model } from "@/store/survey"
 import { PATCH_FIELDS} from "@/store/model.store"
-import { mapActions, mapState } from "vuex"
+import { mapActions, mapState, mapMutations } from "vuex"
 import { NEW_RESPONSE } from "@/store/survey";
 
 export const responseMixin = {
+  data: () => ({
+  }),
   computed: {
     ...mapState(['previewMode']),
   },
   methods: {
+    ...mapMutations({
+      mergeRecords: 'jv/mergeRecords'
+    }),
     ...mapActions({
       patch: PATCH_FIELDS,
       newResponse: NEW_RESPONSE
     }),
-    getResponse(question) {
+    getResponse(question, selectedSubmission) {
       console.debug('getting response...')
-      if (!question || !this.selectedSubmission) {
+      if (!question || !selectedSubmission) {
         console.debug('missing one of question or selectedSubmission', this.question, this.selectedSubmission)
-        return undefined
+        return undefined;
       }
       console.debug("now i have both", this.question, this.selectedSubmission)
       let relationships = {
@@ -29,34 +34,28 @@ export const responseMixin = {
         },
         submission: {
           data: {
-            id: this.selectedSubmission.id,
+            id: selectedSubmission.id,
             type: submissionModel
           }
         }
       }
       let existingResponses = this.$store.getters['jv/get']({_jv: {
         type: model,
-        relationships
-      }})
+      }}, `$[?(@.question.id=='${question.id}' && @.submission.id=='${selectedSubmission.id}')]`)
+      console.log('existing responses', existingResponses)
       let existingResponse = Object.values(existingResponses)[0]
       if (existingResponse?.id) {
-        console.debug("found an existing response", existingResponse)
-        return Promise.resolve(utils.deepCopy(existingResponse))
+        return utils.deepCopy(existingResponse)
       }
       // if there's not one, create a new one
       console.debug("getting a new response")
-      return new Promise((res, rej) => {
-        this.newResponse({relationships}).then((data) => {
-          res(utils.deepCopy(data))
-        }).catch(rej);
-      });
+      return utils.deepCopy(this.newResponse({relationships}));
     },
     saveResponse(response){
       // saving the response only
-      console.log("attempting to save response", response)
-      if (!this.prevewMode && response.id) {
+      if (!this.prevewMode) {
         // only save if not in preview mode and if the response was already saved!
-        return this.patch({model, item: response, fields: ['response'], selected: false})
+        return this.mergeRecords(response);
       }
     }
   }
