d5857e9c84f65569b5d79793e514ee440d53708f
Henry <henry.balen@chicon.org>
2021-11-21 16:23:32 -0500

    mailing WIP

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,18 +3,20 @@
 # stored in the DB
 #
 class ApplicationMailer < ActionMailer::Base
-  default from: Proc.new { from_email },
-          reply_to: Proc.new { reply_to_email }
+  default from: 'planorama@chicon.org'
+
+  # default from: Proc.new { from_email },
+  #         reply_to: Proc.new { reply_to_email }
 
   layout 'mailer'
 
   private
 
-  def from_email
-    @from_email ||= ConfigService.value('email_from_address')
-  end
-
-  def reply_to_email
-    @reply_to_email ||= ConfigService.value('email_reply_to_address')
-  end
+  # def from_email
+  #   @from_email ||= ConfigService.value('email_from_address')
+  # end
+  #
+  # def reply_to_email
+  #   @reply_to_email ||= ConfigService.value('email_reply_to_address')
+  # end
 end
