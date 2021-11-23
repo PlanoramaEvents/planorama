c7f44b95abff5fe42eb7b3ab6855398ebf9dba3a
Henry <henry.balen@chicon.org>
2021-11-23 07:51:58 -0500

    move some modules into shared

---
+++ app/javascript/shared/model-field.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <b-form-group
+      v-if="type !== 'checkbox'"
+      :id="groupId"
+      :label="label"
+      :label-for="inputId"
+    >
+      <b-form-input
+        v-if="type === 'text' || type === 'email'"
+        :id="inputId"
+        :value="value"
+        @input="$emit('input', $event)"
+        :type="type"
+        :disabled="!editable"
+      ></b-form-input>
+      <b-form-textarea
+        v-if="type === 'textarea'"
+        :id="inputId"
+        :value="value"
+        @input="$emit('input', $event)"
+        :disabled="!editable"
+        :maxlength="fieldOptions.maxlength || 2000"
+      ></b-form-textarea>
+    </b-form-group>
+    <b-form-checkbox
+      v-if="type === 'checkbox'"
+      :id="inputId"
+      :checked="value"
+      @change="$emit('input', $event)"
+      :disabled="!editable"
+    > {{ label }} </b-form-checkbox>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: "ModelField",
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+    value: { },
+    type: {
+      type: String,
+      default: "text"
+    },
+    fieldOptions: {
+      type: Object,
+      default: () => ({})
+    },
+    idPrefix: {
+      type: String,
+      default: 'id'
+    },
+    stateless: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    inputId() {
+      return `${this.idPrefix}-input-${this.label.toLowerCase().replace(' ', '-')}`
+    },
+    groupId() {
+      return `${this.inputId}-group`;
+    },
+    editable() {
+      if(this.stateless) return true;
+      return this.$store.state.editable;
+    },
+  }
+}
+</script>
---
+++ app/javascript/shared/toast-mixin.js
@@ -0,0 +1,43 @@
+export const toastMixin = {
+  methods: {
+    success_toast(text) {
+      this.$bvToast.toast(text, {
+        variant: 'success',
+        title: 'Success',
+        // toaster: 'planotoaster'
+      })
+    },
+    error_toast(text) {
+      this.$bvToast.toast(text, {
+        variant: 'danger',
+        title: 'Something went wrong',
+        // toaster: 'planotoaster'
+      })
+    },
+    toastCallback(method, success_message) {
+      method().then(() => this.success_toast(success_message))
+        .catch((error) => this.error_toast(error.message))
+    },
+    toastSuccessFailure(success_message) {
+      return {
+        success: () => this.success_toast(success_message),
+        failure: (error) => this.error_toast(error.message)
+      }
+    },
+    toastPromise(promise, success_text) {
+      return new Promise((res, rej) => {
+        promise.then((item) => {
+          this.success_toast(success_text);
+          res(item);
+        })
+        .catch((error) => {
+          console.error(error)
+          this.error_toast(error.message)
+          rej(error);
+        })
+      });
+    }
+  }
+}
+
+export default toastMixin;
