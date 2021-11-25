459727e9518c2acfb8c2fba6f52537b980f98bcc
Gail Terman <gterman@gmail.com>
2021-11-24 20:15:52 -0500

    add jest and some example unit tests

---
+++ test/javascript/examples/test-demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="count++">Add up</button>
+    <p>Total clicks: {{ count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestCounter",
+  data: () => ({
+    count: 0
+  })
+}
+</script>
+
+<style>
+
+</style>
