12b48ab3552fe6c8d2570e519a7daaf6eab64691
Gail Terman <gterman@gmail.com>
2021-12-08 21:41:24 -0500

    fix survey settings

--- app/javascript/surveys/survey.mixin.js
+++ app/javascript/surveys/survey.mixin.js
@@ -1,6 +1,6 @@
-import {mapGetters} from 'vuex';
+import {mapGetters, mapActions} from 'vuex';
 import toastMixin from '../shared/toast-mixin';
-import { SAVE, SELECT, SELECTED, FETCH_SELECTED, DELETE, UNSELECT } from '../store/model.store';
+import { SAVE, SELECT, SELECTED, FETCH_SELECTED, DELETE, UNSELECT, PATCH_FIELDS } from '../store/model.store';
 import { SURVEY_SAVE_SUCCESS, SURVEY_SAVE_SUCCESS_DELETE } from '../constants/strings'
 import { surveyModel as model} from '@/store/survey';
 import { getOrderedRelationships } from '../utils/jsonapi_utils';
@@ -25,6 +25,9 @@ export const surveyMixin = {
     },
   },
   methods: {
+    ...mapActions({
+      patch: PATCH_FIELDS
+    }),
     saveSurvey(newSurvey, success_text = SURVEY_SAVE_SUCCESS) {
       if (!newSurvey) {
         newSurvey = this.survey;
@@ -48,6 +51,10 @@ export const surveyMixin = {
     },
     getSurveyPages(survey) {
       return getOrderedRelationships('pages', survey);
+    },
+    patchSurveyField(survey, field, success_text = SURVEY_SAVE_SUCCESS) {
+      console.debug("attempting to patch survey field", field, "with value", survey[field])
+      this.toastPromise(this.patch({model, item: survey, fields: [field]}), success_text)
     }
   }
 }
