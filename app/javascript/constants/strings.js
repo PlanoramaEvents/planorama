264083a6737c18bb0f0e80d5dfce4fdb77a0031e
annA <597368+arbradley@users.noreply.github.com>
2021-12-15 22:38:52 -0500

    Update strings.js
    changed error messaging to have real words, and added some new messages

--- app/javascript/constants/strings.js
+++ app/javascript/constants/strings.js
@@ -1,9 +1,9 @@
 module.exports = {
     // login page
-    LOGIN_401: "You have an incorrect username or password.",
-    LOGIN_MISSING_PASSWORD: "You must enter a password.",
+    LOGIN_401: "You have entered an incorrect username or password.",
+    LOGIN_MISSING_PASSWORD: "You must enter a valid password.",
     LOGIN_INVALID_FIELDS: "You must provide all of your login information.",
-    MISSING_EMAIL: "You must enter an email address.",
+    MISSING_EMAIL: "You must enter a valid email address.",
     NOT_AN_EMAIL: "Email address needs to be in the correct format e.g. xyz@test.com",
     LOGIN_SHORT_PASSWORD: "Your password must be at least 6 characters long.",
     LOGIN_PASSWORDS_DO_NOT_MATCH: "Your passwords do not match.",
@@ -15,26 +15,67 @@ module.exports = {
     LOGIN_PASSWORD_CHANGED: "You successfully changed your password.",
     LOGIN_TOKEN_EXPIRED: (resetPasswordLink) => `The password reset link you used is no longer valid. Please request another link here: ${resetPasswordLink}`,
     LOGIN_CLICK_TO_AGREE: "By clicking ‘Log In’ below, I agree to Chicon 8 storing and using my personal data as documented in the",
+        
+    // toast titles
+    ERROR_TOAST_TITLE: "Error",
+    SUCCESS_TOAST_TITLE: "Success",
+    
+    //survey actions
     SURVEY_SAVE_SUCCESS: "Survey successfully saved.",
+    SURVEY_SAVE_ERROR: (errorCode) => `Survey failed to be saved. \n  Error code: ${errorCode}`,
     SURVEY_CONFIRM_PUBLISH: "You are about to publish this survey. This will make the survey visible to the public and allow responses to be collected. Are you sure you want to publish this survey?",
     SURVEY_CONFIRM_CLOSE: "You are about to close this survey. This will no longer allow any responses to be collected. Are you sure you want to close this survey?",
     SURVEY_CONFIRM_DELETE: "Are you sure you want to permanently delete this survey? This cannot be undone.",
-    SURVEY_CONFIRM_DELETE_PAGE_1: "Deleting a page also deletes the questions and response options it contains.",
-    SURVEY_CONFIRM_DELETE_PAGE_2: "To preserve the questions, choose \"Merge page up\" from the page options.",
     SURVEY_SAVE_SUCCESS_PUBLISH: "Survey successfully published.",
+    SURVEY_SAVE_ERROR_PUBLISH: (errorCode) => `Survey failed to be published. \n  Error code: ${errorCode}`,
     SURVEY_SAVE_SUCCESS_CLOSE: "Survey successfully closed.",
+    SURVEY_SAVE_ERROR_CLOSE: (errorCode) => `Survey failed to be closed. \n  Error code: ${errorCode}`,
     SURVEY_SAVE_SUCCESS_DELETE: "Survey successfully deleted.",
+    SURVEY_SAVE_ERROR_DELETE: (errorCode) => `Survey failed to be deleted. \n  Error code: ${errorCode}`,
+    
+    // survey results actions
     SURVEY_RESULTS_CLEAR_SUCCESS: "Results successfully cleared.",
+    SURVEY_RESULTS_CLEAR_ERROR: (errorCode) => `Results failed to be cleared. \n  Error code: ${errorCode}`,
     SURVEY_RESULTS_CLEAR_CONFIRM: "This will permanently delete all responses to this survey. Are you sure you wish to clear all responses?",
     SURVEY_RESULTS_FREEZE_SUCCESS: "Response edits successfully frozen.",
+    SURVEY_RESULTS_FREEZE_ERROR: (errorCode) => `Response edits failed to be frozen. \n  Error code: ${errorCode}`,
     SURVEY_RESULTS_UNFREEZE_SUCCESS: "Response edits successfully unfrozen.",
-    SURVEY_DEFAULT_THANK_YOU: "Your response has been recorded.",
+    SURVEY_RESULTS_UNFREEZE_ERROR: (errorCode) => `Response edits failed to be unfrozen. \n  Error code: ${errorCode}`,
+    
+    //survey defaults
+    SURVEY_DEFAULT_THANK_YOU: "Your responses have been recorded.",
+    
+    //generic app wide 
     NOT_IMPLEMENTED: "This feature is not yet implemented. Check back soon!",
-    ADMIN_ADD_USER_SUCCESS: (name) => `${name} successfully added.`,
-    IEA_FAILURE_TO_SIGN: "As a member of Staff you can not proceed without agreeing to the ethics and data privacy statments.",
-    QUESTION_ORDER_SAVE_SUCCESS: "Gail should not write copy also question order saved",
-    QUESTION_SAVE_SUCCESS: "Gail should not write copy also queston saved",
-    ANSWER_ORDER_SAVE_SUCCESS: "Gail should not write copy also answer order saved",
-    ANSWER_SAVE_SUCCESS: "Gail should not write copy also answer saved",
-    SAVE_SUCCESS: (model) => `${model.substring(0, 1).toUpperCase()}${model.substring(1)} saved.`
+    ADMIN_ADD_USER_SUCCESS: (name) => `${name} successfully added to Planorama.`,
+    IEA_FAILURE_TO_SIGN: "As a member of Staff you can not proceed without agreeing to the ethics and data privacy statments.",   
+    SAVE_SUCCESS: (model) => `${model.substring(0, 1).toUpperCase()}${model.substring(1)} sucessfully saved.`,
+    SAVE_ERROR: (model) => `${model.substring(0, 1).toUpperCase()}${model.substring(1)} failed to be saved.`,
+    
+    //question actions
+    QUESTION_ORDER_SAVE_SUCCESS: "Question reorder successfully saved",
+    QUESTION_ORDER_SAVE_ERROR: (errorCode) => `Question reorder failed to be saved. \n  Error code: ${errorCode}`,
+    QUESTION_SAVE_SUCCESS: "Question successfully saved",
+    QUESTION_SAVE_ERROR: (errorCode) => `Question failed to be saved. \n  Error code: ${errorCode}`,
+    QUESTION_DELETE_SUCCESS: "Question successfully deleted",
+    QUESTION_DELETE_ERROR: (errorCode) => `Question failed to be deleted. \n  Error code: ${errorCode}`,
+    QUESTION_DUPLICATE_SUCCESS: "Question successfully duplicated",
+    QUESTION_DUPLICATE_ERROR: (errorCode) => `Question failed to be duplicated. \n  Error code: ${errorCode}`,
+    
+    //answer actions
+    ANSWER_ORDER_SAVE_SUCCESS: "Answer reorder sucessfully saved",
+    ANSWER_ORDER_SAVE_ERROR: (errorCode) => `Answer reorder failed to be saved. \n  Error code: ${errorCode}`,
+    ANSWER_SAVE_SUCCESS: "Answer sucessfully saved",
+    ANSWER_SAVE_ERROR: (errorCode) => `Answer reorder failed to be saved. \n  Error code: ${errorCode}`,
+    
+    //page actions 
+    PAGE_DELETE_SUCCESS: "Page successfully deleted",
+    PAGE_DELETE_ERROR: (errorCode) => `Page failed to be deleted. \n  Error code: ${errorCode}`,
+    PAGE_MERGE_SUCCESS: "Page successfully merged",
+    PAGE_MERGE_ERROR: (errorCode) => `Page failed to be merged. \n  Error code: ${errorCode}`,
+    SURVEY_CONFIRM_DELETE_PAGE_1: "Deleting a page also deletes the questions and response options it contains.",
+    SURVEY_CONFIRM_DELETE_PAGE_2: "To preserve the questions, choose \"Merge page up\" from the page options."
+
+
+    // put in all the adds
 }
