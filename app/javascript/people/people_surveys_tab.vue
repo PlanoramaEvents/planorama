9596a41cb47d918b32e9f337bea1df9ccf4377dc
Henry <henry.balen@chicon.org>
2022-01-09 11:00:45 -0500

    fix merge

--- app/javascript/people/people_surveys_tab.vue
+++ app/javascript/people/people_surveys_tab.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h2>Surveys</h2>
+<!--    <ul>
+      <li v-for="role of selected.person_roles" :key="role.id">
+        {{ role.role }}
+      </li>
+    </ul>
+    <h2>Mail History</h2>
+    <pre>{{ selected.mail_histories }}</pre>-->
     <survey-table></survey-table>
  </div>
 </template>
