27ee5185dcc27550be986f8d73b114edebb5539d
Henry <henry.balen@chicon.org>
2022-01-10 15:56:25 -0500

    fix assets for editor pligin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-//= require_tree ckeditor
+// require_tree ./ckeditor
 //
