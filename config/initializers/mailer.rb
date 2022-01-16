192b1c574dd114927d9eaa67bab6779423bf9faf
Henry <henry.balen@chicon.org>
2022-01-15 19:56:46 -0500

    fixes for mail catcher

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -7,12 +7,12 @@ Rails.application.config.action_mailer.tap do |action_mailer|
     action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     action_mailer.delivery_method = :smtp
     action_mailer.perform_deliveries = true
-    action_mailer.raise_delivery_errors = false
 
     action_mailer.tap do |action_mailer|
+      action_mailer.raise_delivery_errors = true
       action_mailer.smtp_settings = {
         address:              ENV.fetch("SMTP_SERVER") { 'localhost' },
-        port:                 ENV.fetch("SMTP_PORT") { 10025 },
+        port:                 ENV.fetch("SMTP_PORT") { 1025 },
         domain:               ENV.fetch("SMTP_SERVER") { 'localhost' }
       }
     end
