13cf4f51da737026de3d9a458ee0563591710c28
ralphlevan <ralphlevan@gmail.com>
2022-02-11 19:03:00 -0700

    Out with the old and in with the new agreements code

--- app/javascript/agreements/sign_agreements.vue
+++ app/javascript/agreements/sign_agreements.vue
@@ -1,33 +1,71 @@
 <template>
-  <b-modal id="signature-modal" title="Check Sigs ....">
-    <p class="my-4">
-      TODO: check the sigs
-    </p>
+  <b-modal id='agreements-modal' size="lg"
+           hide-header-close no-close-on-backdrop no-close-on-esc
+           :title="title" @ok="okAction"
+           ok-title="I Agree" cancel-title="I Refuse">
+    <div class="agreements-terms">
+      <slot name="terms">{{terms}}</slot>
+    </div>
+    <div>
+      <footer class="modal-footer">
+        <slot name="footer">
+          <p>By clicking ‘Agree’ below I do hereby agree to abide by the above usage restrictions. I acknowledge that there may be personal, business, and legal implications if I use this system inappropriately.</p>
+        </slot>
+        <button
+            type="button"
+            class="btn-green"
+            @click="close"
+        >
+          Close Modal
+        </button>
+      </footer>
+    </div>
   </b-modal>
 </template>
 
 
 <script>
 import personSessionMixin from '../auth/person_session.mixin';
-import AgreementsModal from "@/components/agreements_modal";
+import {http as axios} from '../http';
+var signedAgreements={};
 
 export default {
   name: "SignAgreements",
-  components: {
-    AgreementsModal,
-  },
   mixins: [personSessionMixin],
+  data() {
+    return {
+      ua:'',
+      terms:'',
+      title:''
+    }
+  },
   methods: {
     check_signatures() {
-      // TODO: @ralph deal with the login here - we have the session from the pesonSession etc
-
-      console.debug("WE ARE GOING TO CHECK SIGS")
-      this.$bvModal.show('signature-modal')
+      for (this.ua in this.currentUser.unsigned_agreements) {
+        if(!signedAgreements[this.ua]) {
+          this.title=this.currentUser.unsigned_agreements[this.ua].title;
+          this.terms=this.currentUser.unsigned_agreements[this.ua].terms;
+          this.$bvModal.show('agreements-modal');
+          break;
+        }
+      }
+    },
+    okAction() {
+      signedAgreements[this.ua]=true;
+      //TODO: http://localhost:3000/agreement/sign/{{agreement-id}}
+      axios.put('/agreement/sign/'+this.ua);
+      setTimeout(() => { this.check_signatures(); }, 500);
+    },
+    close() {
+      alert('modal closed')
     }
   }
 }
 </script>
 
 <style>
-
+.agreements-terms {
+  max-height: 50vh;
+  overflow-y: auto;
+}
 </style>
