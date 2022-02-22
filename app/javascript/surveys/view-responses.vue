6c1c0e1fcb241c7cd4f411c4f48c67c3906dbb0e
Henry <henry.balen@chicon.org>
2022-02-22 18:49:48 -0500

    Survey response WIP

--- app/javascript/surveys/view-responses.vue
+++ app/javascript/surveys/view-responses.vue
@@ -5,8 +5,10 @@
     <ul>
       <li v-for="submission of submissions" :key="submission.id">
         {{new Date(submission.created_at).toLocaleString()}} by {{submission.submitter}}
-        <ul v-if="getResponses(submission).length">
-          <li v-for="response of getResponses(submission)" :key="response.id">{{response.question_text}}: {{response.response.text || response.response.answers}}</li>
+        <ul>
+          <li v-for="response of submission.responses" :key="response.id">
+            {{response.question_text}}: {{response.response.text || response.response.answers}}
+          </li>
         </ul>
       </li>
     </ul>
@@ -30,31 +32,21 @@ export default {
   }),
   computed: {
     downloadLink() {
-      return `/surveys/${this.survey?.id}/submissions.xlsx`
+      return `/survey/${this.selectedSurveyId}/submissions.xlsx`
     },
     filename() {
-      return `survey_${this.survey?.id}_responses.xlsx`
+      return `survey_${this.selectedSurveyId}_responses.xlsx`
     },
     submissions() {
-      if (!this.survey) {
+      if (!this.selectedSurveyId) {
         return [];
       }
-      return Object.values(this.$store.getters['jv/get']({_jv: {type: submissionModel}}), `$[(?@.survey.id=='${this.survey.id}')]`)
+      return Object.values(this.$store.getters['jv/get']({_jv: {type: submissionModel}}), `$[(?@.survey.id=='${this.selectedSurveyId}')]`)
     },
     selectedSurveyId() {
       return this.$store.state.selected[surveyModel];
     }
-  },
-  methods: {
-    getResponses(submission) {
-      return getOrderedRelationships('responses', submission)
-    }
-  },
-  mounted() {
-    if(this.survey) {
-      this.getSubmissionsForSurvey(this.survey);
-    }
-  },
+  }
   watch: {
     selectedSurveyId(newVal, oldVal) {
       if (newVal && (!oldVal || newVal !== oldVal)) {
