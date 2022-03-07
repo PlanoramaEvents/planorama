fdea4d2c81b9108324b94974dc87ded914e6e229
Henry <henry.balen@chicon.org>
2022-03-07 17:16:21 -0500

    Survey Fixes WIP

--- app/javascript/surveys/survey_table.vue
+++ app/javascript/surveys/survey_table.vue
@@ -19,11 +19,11 @@ ISSUE - template links don't work
       <tooltip-overflow :title="item.updated_by ? item.updated_by.name : '?????'">{{item.updated_by ? item.updated_by.name : '?????'}}</tooltip-overflow>
     </template>
     <template #cell(preview)="{ item }">
-      <a :to="previewLink(item)" target="_blank">Preview</a>
+      <a :href="previewLink(item)" target="_blank">Preview</a>
     </template>
     <template #cell(surveyLink)="{ item }">
       <small v-if="!item.anonymous"><i>This survey is not anonymous</i></small>
-      <a v-if="item.anonymous" :to="surveyLink(item)" target="_blank">{{ surveyLink(item) }}</a>
+      <a v-if="item.anonymous" :href="surveyLink(item)" target="_blank">{{ surveyLink(item) }}</a>
     </template>
   </table-vue>
 </template>
@@ -35,6 +35,7 @@ import TooltipOverflow from '../shared/tooltip-overflow';
 import { survey_columns as columns } from './survey';
 import { mapActions } from 'vuex';
 import { NEW_SURVEY } from '@/store/survey';
+import { surveyMixin }from '@mixins';
 
 export default {
   name: 'SurveyTable',
@@ -45,6 +46,9 @@ export default {
   props: {
     defaultFilter: String
   },
+  mixins: [
+    surveyMixin
+  ],
   data() {
     return {
       columns
@@ -53,17 +57,21 @@ export default {
   methods: {
     ...mapActions({newSurvey: NEW_SURVEY}),
     previewLink(item) {
-      return `/surveys/${item.id}/preview`;
+      return `/#/surveys/${item.id}/preview`;
     },
     surveyLink(item) {
       // TODO add authenticity key to stop robots?
-      return `/surveys/${item.id}`;
+      // This link is on the survey listing page and not accessible without login ...
+      return `/#/surveys/${item.id}`;
     },
     onNew() {
       this.newSurvey().then((survey) => {
         this.$router.push({path: `/surveys/edit/${survey.id}`})
       });
     }
+  },
+  mounted() {
+    this.unselectSurvey();
   }
 }
 </script>
