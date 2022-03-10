141d75629d6c0183caacbd354baf1322580e70ed
Henry <henry.balen@chicon.org>
2022-03-10 13:50:09 -0500

    settings WIP

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,10 +6,20 @@ class SettingsController < ApplicationController
   #
   def index
     # 1. model information
+    Rails.application.eager_load! # for apps that do dynamic loading so we have all the classes
+    clazzes = ApplicationRecord.descendants.collect(&:name).collect{|c| c.constantize}
+    enums = {}
+    clazzes.each do |c|
+      if c.defined_enums.size > 0
+        enums[c.name] = c.defined_enums.transform_values{|v| v.values }
+      end
+    end
+
     # 2. Aagreement Types
     # 3. list of configs
     settings = {
-      models: {},
+      # mdl: ApplicationRecord.descendants.collect(&:name),
+      enums: enums,
       # TODO: move agreement types to DB
       # At the moment we have 2 types of agreement
       agreement_types: [
