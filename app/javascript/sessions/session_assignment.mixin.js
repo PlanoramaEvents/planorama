d678a95cad91dc318698b1da1394beeb4a1a18b0
Henry <henry.balen@chicon.org>
2022-03-22 14:25:46 -0400

    tweaks for sesssion selection

--- app/javascript/sessions/session_assignment.mixin.js
+++ app/javascript/sessions/session_assignment.mixin.js
@@ -1,6 +1,6 @@
 import {mapState, mapActions } from 'vuex';
 import { toastMixin, modelMixin, personSessionMixin } from "@/mixins";
-import { NEW_SESSION_ASSIGNMENT } from "../store/session_assignment.store";
+import { NEW_SESSION_ASSIGNMENT, SESSION_EXPRESS_INTEREST, SESSION_UNEXPRESS_INTEREST } from "../store/session_assignment.store";
 import { SAVE, DELETE } from "../store/model.store";
 
 import {
@@ -15,23 +15,23 @@ import {
 export const sessionAssignmentMixin = {
   mixins: [modelMixin, toastMixin, personSessionMixin],
   methods: {
-    ...mapActions({ newSessionAssignment: NEW_SESSION_ASSIGNMENT}),
-    expressInterest(session, person = null, success_text = SESSION_INTEREST_SUCCESS, error_text = SESSION_INTEREST_ERROR) {
-      if (!person) {
-        person = this.currentUser;
-      }
-      // change
+    ...mapActions({
+      newSessionAssignment: NEW_SESSION_ASSIGNMENT,
+      sessionExpressInterest: SESSION_EXPRESS_INTEREST,
+      sessionUnExpressInterest: SESSION_UNEXPRESS_INTEREST,
+    }),
+    expressInterest(session, success_text = SESSION_INTEREST_SUCCESS, error_text = SESSION_INTEREST_ERROR) {
       return this.toastPromise(
-        this.newSessionAssignment({session: session, person_id: person.id }), success_text, error_text
+        this.sessionExpressInterest({session: session}),
+        success_text,
+        error_text
       );
     },
     removeInterest(assignment, success_text = SESSION_INTEREST_REMOVE_SUCCESS, error_text = SESSION_INTEREST_REMOVE_ERROR) {
       return this.toastPromise(
-        // change
-        this.$store.dispatch(
-          DELETE, {model: this.model, itemOrId: assignment}
-        ),
-        success_text, error_text
+        this.sessionUnExpressInterest({assignment: assignment}),
+        success_text,
+        error_text
       );
     }
   }
--- app/javascript/store/session_assignment.store.js
+++ app/javascript/store/session_assignment.store.js
@@ -1,7 +1,12 @@
+import {http} from "@/http"
+
 import { NEW } from './model.store';
 import personSessionMixin from "../auth/person_session.mixin";
 
 export const NEW_SESSION_ASSIGNMENT = 'NEW SESSION ASSIGNMENT';
+
+export const SESSION_EXPRESS_INTEREST = 'SESSION EXPRESS INTEREST';
+export const SESSION_UNEXPRESS_INTEREST = 'SESSION UNEXPRESS INTEREST';
 
 export const sessionAssignmentModel = 'session_assignment';
 
@@ -12,7 +17,6 @@ export const sessionAssignmentEndpoints = {
 export const sessionAssignmentStore = {
   actions: {
     [NEW_SESSION_ASSIGNMENT] ({dispatch}, {session, person_id}) {
-      // TODO: change
       let newAssignment = {
         person_id: person_id,
         session_id: session.id,
@@ -20,7 +24,24 @@ export const sessionAssignmentStore = {
       }
       return dispatch(NEW, {model: sessionAssignmentModel, selected: false, ...newAssignment});
     },
-    // TODO: add a not interested action etc
+    [SESSION_EXPRESS_INTEREST] ({dispatch}, {session}) {
+      return new Promise((res, rej) => {
+        http.put(`/session/${session.id}/express_interest`).then((assignment) => {
+          res(assignment);
+        }).catch((error) => {
+          res({});
+        })
+      })
+    },
+    [SESSION_UNEXPRESS_INTEREST] ({dispatch}, {assignment}) {
+      return new Promise((res, rej) => {
+        http.put(`/session_assignment/${assignment.id}/unexpress_interest`).then((assignment) => {
+          res(assignment);
+        }).catch((error) => {
+          res({});
+        })
+      })
+    }
   },
   selected: {
     [sessionAssignmentModel]: undefined
