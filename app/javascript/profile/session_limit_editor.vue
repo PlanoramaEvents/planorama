e4102a4b650adff060a32c281a13fd5de1495cd0
Henry <henry.balen@chicon.org>
2022-04-11 11:22:15 -0400

    fix limit validation

--- app/javascript/profile/session_limit_editor.vue
+++ app/javascript/profile/session_limit_editor.vue
@@ -1,19 +1,35 @@
 <template>
-  <b-form-input
-    v-model="limit"
-    type='number'
-    @change="onChange"
-    min="0"
-  ></b-form-input>
+  <div v-if="my_limit">
+    <validation-provider
+      rules="numeric"
+      name="Limit"
+      :skipIfEmpty="true"
+      v-slot="{ valid, errors }"
+    >
+      <!-- Validate works if the type is text, if type is number then it does not -->
+      <input
+        v-model="my_limit.max_sessions"
+        type='text'
+        maxlength="4" size="4"
+        @blur="onChange(my_limit.max_sessions)"
+      ></input>
+      <div class="invalid-message">{{ errors[0] }}</div>
+    </validation-provider>
+  </div>
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver} from 'vee-validate';
 import settingsMixin from "@/store/settings.mixin";
 import sessionLimitMixin from '../store/session_limit.mixin'
 import personSessionMixin from '../auth/person_session.mixin';
 
 export default {
   name: "SessionLimitEditor",
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   mixins: [
     settingsMixin,
     personSessionMixin,
@@ -48,10 +64,11 @@ export default {
   },
   methods: {
     onChange(newVal) {
-      // Do not do anything if the value is not a number
-      if (!isNaN(parseInt(newVal))) {
+      // Do not do anything if the value is not a positive number
+      const num = Number(newVal);
+      if (Number.isInteger(num) && num > 0) {
         if (this.my_limit) {
-          this.my_limit.max_sessions = newVal
+          this.my_limit.max_sessions = num
           this.save(this.my_limit).then(
             (data) => {
               this.my_limit = data
@@ -61,7 +78,7 @@ export default {
           let candidate = {
             person_id: this.currentUser.id,
             day: this.day,
-            max_sessions: newVal
+            max_sessions: num
           }
           this.create_session_limit(candidate).then(
             (data) => {
