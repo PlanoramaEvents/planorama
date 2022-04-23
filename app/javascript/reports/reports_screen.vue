a64369d193284401566a8ace7e29d1ea241e632e
Henry <henry.balen@chicon.org>
2022-04-23 14:47:43 -0400

    add report links

---
+++ app/javascript/reports/reports_screen.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mb-3">
+    This are interim reports:
+    <ul>
+      <li>
+        <a href="/report/participant_selections" target="_blank">Participant Selections</a>
+      </li>
+      <li>
+        <a href="/report/session_selections" target="_blank">Session Selections</a>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReportsScreen"
+}
+</script>
+
+<style>
+
+</style>
