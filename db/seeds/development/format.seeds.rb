0d3ee63168b24bf7362fefb121a92649494a8802
Leane Verhulst <lverhulst@gmail.com>
2022-05-13 00:07:35 -0500

    Created some new seed files for dev and staging env

---
+++ db/seeds/development/format.seeds.rb
@@ -0,0 +1,16 @@
+return if Format.count > 0
+
+Format.create(
+  name: 'Panel',
+  position: 1
+)
+
+Format.create(
+  name: 'Talk',
+  position: 2
+)
+
+Format.create(
+  name: 'Workshop',
+  position: 3
+)
