3f5c7fe0e405465d9d71ae5037830c50a9c8e4f0
Leane Verhulst <lverhulst@gmail.com>
2022-05-22 10:31:57 -0500

    Updated dev session seed

--- db/seeds/development/session.seeds.rb
+++ db/seeds/development/session.seeds.rb
@@ -2,6 +2,7 @@ p "Start development session seeds"
 
 return if Session.count > 0
 
+
 unless Format.find_by_name('Panel')
   Format.create!(
     name: 'Panel',
@@ -16,17 +17,58 @@ unless Format.find_by_name('Talk')
   )
 end
 
-panel = Format.find_by_name('Panel')
-talk = Format.find_by_name('Talk')
-
-100.times.each do |i|
-  format = i % 2 ? panel : talk
-  Session.create(
-    title: Faker::Book.unique.title,
-    description: Faker::TvShows::TheExpanse.quote,
-    format_id: format.id,
-    audience_size: 100
+unless Area.find_by_name('Fandom')
+  Area.create!(
+    name: 'Fandom',
+    position: 8
   )
 end
 
-p "End development session seeds"
+panel = Format.find_by_name('Panel')
+talk = Format.find_by_name('Talk')
+#area = Area.find_by_name('Fandom')
+
+90.times.each do |i|
+  format = i % 2 ? panel : talk
+  area = Area.all.sample
+  s = Session.create!(
+    title: Faker::Book.unique.title,
+    description: Faker::TvShows::TheExpanse.quote,
+    format_id: format.id,
+    audience_size: 100,
+    duration: 30 * ((i % 2) + 1),
+    open_for_interest: Faker::Boolean.boolean(true_ratio: 0.8),
+    require_signup: Faker::Boolean.boolean(true_ratio: 0.2),
+    item_notes: "Test of the item_notes field",
+    participant_notes: "Test of the participant_notes field",
+    instructions_for_interest: "Test of the instructions_for_interest field"
+  )
+  SessionArea.create!(
+    session_id: s.id,
+    area_id: area.id
+  )
+end
+
+10.times.each do |i|
+  format = Format.all.sample
+  area = Area.all.sample
+  s = Session.create!(
+    title: Faker::Book.unique.title,
+    description: Faker::TvShows::TheExpanse.quote,
+    format_id: format.id,
+    audience_size: 100,
+    duration: 30 * ((i % 2) + 1),
+    open_for_interest: Faker::Boolean.boolean(true_ratio: 0.8),
+    require_signup: Faker::Boolean.boolean(true_ratio: 0.2),
+    item_notes: "Test of the item_notes field",
+    participant_notes: "Test of the participant_notes field",
+    instructions_for_interest: "Test of the instructions_for_interest field"
+  )
+  SessionArea.create!(
+    session_id: s.id,
+    area_id: area.id
+  )
+end
+
+
+p "End development session seeds: #{Session.count}"
