1e4dfc08eef5faae1757ddd0a0cb8b9618b2927a
Henry <henry.balen@chicon.org>
2022-05-24 11:27:04 -0400

    split day display

--- app/javascript/schedule/schedule_day.vue
+++ app/javascript/schedule/schedule_day.vue
@@ -17,11 +17,13 @@
     ref="dayRoomGrid"
     :events="events"
     @event-drop="onEventDrop"
-    @event-focus="onEventFocus"
-    @event-mouse-enter="onEventEnter"
-    @event-mouse-leave="onEventLeave"
-    cellContextmenu
   >
+  <!--
+  @event-focus="onEventFocus"
+  @event-mouse-enter="onEventEnter"
+  @event-mouse-leave="onEventLeave"
+  cellContextmenu
+  -->
   <!-- hover
     event-mouse-enter
     event-mouse-leave
@@ -37,7 +39,7 @@
       <div class="d-flex flex-column">
         <div class="d-flex flex-row p-1 justify-content-between">
           <small class="event-time">
-            {{ formatLocaleJsDate(event.start) }} - {{ formatDatetime(addMinutes(event.start, event.duration_mins)) }}
+            {{ formatDatetime(event.actual_start) }} - {{ formatDatetime(event.actual_end) }}
           </small>
           <b-icon-x @click="onDelete(event)"></b-icon-x>
         </div>
@@ -108,15 +110,27 @@ export default {
         let start_time = DateTime.fromISO(session.start_time).setZone(this.timezone)
         let end_time = DateTime.fromISO(session.end_time).setZone(this.timezone)
         // check it is for the right day
-        if (start_time.toFormat('yyyy-MM-dd') == this.selectedDate) {
+        if ((start_time.toFormat('yyyy-MM-dd') == this.selectedDate) || (end_time.toFormat('yyyy-MM-dd') == this.selectedDate)) {
+        // if ((start_time.toFormat('yyyy-MM-dd') == this.selectedDate)) {
           // let new_event = this.$refs['dayRoomGrid'].createEvent(
+          let display_end_time = end_time
+          let display_start_time = start_time
+          if (end_time.toFormat('yyyy-MM-dd') != this.selectedDate) {
+            display_end_time = start_time.endOf('day')
+          }
+          if (start_time.toFormat('yyyy-MM-dd') != this.selectedDate) {
+            display_start_time = end_time.startOf('day')
+          }
+          // let display_end_time = end_time.toFormat('yyyy-MM-dd') != this.selectedDate ? start_time.set({hour: 11, minute: 59}) : end_time
           let new_event = {
-            start: start_time.toFormat('yyyy-MM-dd HH:mm'),
-            end: end_time.toFormat('yyyy-MM-dd HH:mm'),
+            start: display_start_time.toFormat('yyyy-MM-dd HH:mm'),
+            end: display_end_time.toFormat('yyyy-MM-dd HH:mm'),
             title: session.title,
             split: session.room_id,
             id: session.id,
-            duration_mins: session.duration_mins
+            duration_mins: session.duration_mins,
+            actual_start: start_time,
+            actual_end: end_time
           }
           res.push(new_event)
         }
