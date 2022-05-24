effb9e63e4814f4a3a8103c177156021dc10172d
Henry <henry.balen@chicon.org>
2022-05-24 10:40:38 -0400

    sort rooms by name for now

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,5 +2,5 @@ class RoomsController < ResourceController
   SERIALIZER_CLASS = 'RoomSerializer'.freeze
   POLICY_CLASS = 'RoomPolicy'.freeze
   POLICY_SCOPE_CLASS = 'RoomPolicy::Scope'.freeze
-  DEFAULT_SORTBY = 'sort_order'
+  DEFAULT_SORTBY = 'rooms.name'
 end
