d657f049443a3105c8ee7138a535674f9e91c993
Henry <henry.balen@chicon.org>
2022-07-12 18:18:32 -0400

    WIP

--- app/services/snapshot_service.rb
+++ app/services/snapshot_service.rb
@@ -4,12 +4,24 @@ module SnapshotService
   # Take snapshot
   # For each participant got through and snapshot their schedule
   # SessionService.draft_schedule_for(person:)
-  def self.takeSnapshots
-    participants = Person
-                    .where("con_state != 'declined' && con_state != 'rejected'")
-                    .where('id in (select person_id from person_schedules)')
-    participants.all do |participant|
-      snapshot = SessionService.draft_schedule_for(person: participant)
+  def self.takeSnapshots(schedule_snapshot:)
+    return unless schedule_snapshot
+
+    Person.transaction do
+      participants = Person
+                      .where("con_state != 'declined' AND con_state != 'rejected'")
+                      .where('id in (select person_id from person_schedules)')
+
+      participants.each do |participant|
+        # Generate the snapshot
+        snapshot = SessionService.draft_schedule_for(person: participant)
+        # save the snapshot
+        PersonScheduleSnapshot.create!(
+          schedule_snapshot: schedule_snapshot,
+          person: participant,
+          snapshot: snapshot.to_json
+        )
+      end
     end
   end
 
