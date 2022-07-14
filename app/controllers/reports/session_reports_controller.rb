c0c3ba15897012c43fd894b1e92b5edd8ad4b4da
Henry <henry.balen@chicon.org>
2022-07-13 21:28:03 -0400

    fix issue with nil start time

--- app/controllers/reports/session_reports_controller.rb
+++ app/controllers/reports/session_reports_controller.rb
@@ -65,7 +65,7 @@ class Reports::SessionReportsController < ApplicationController
         [
           session.title,
           session.area_list.sort.join(';'),
-          FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset),
+          session.start_time ? FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset) : nil,
           session.room.name
         ],
         styles
@@ -108,7 +108,7 @@ class Reports::SessionReportsController < ApplicationController
         [
           session.title,
           session.area_list.sort.join(';'),
-          FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset),
+          session.start_time ? FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset) : nil,
           session.format.name,
           session.session_assignments.select{|a| a.session_assignment_role_type_id == participant.id}.collect{|a| a.person.published_name}.join(';'),
           session.session_assignments.select{|a| a.session_assignment_role_type_id == reserve.id}.collect{|a| a.person.published_name}.join(';')
@@ -372,7 +372,7 @@ class Reports::SessionReportsController < ApplicationController
         [
           session.title,
           session.area_list.sort.join(';'),
-          FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset),
+          session.start_time ? FastExcel.date_num(session.start_time, session.start_time.in_time_zone.utc_offset) : nil,
           session.nbr_assignments,
           6,
           session.session_assignments.select{|a| a.session_assignment_role_type_id == participant.id}.collect{|a| a.person.published_name}.join(';'),
