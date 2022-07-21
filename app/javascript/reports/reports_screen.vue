20ec584e5828e48705ef9d7861e0f1eb1951c15c
Gail Terman <gterman@gmail.com>
2022-07-21 18:54:54 -0400

    PLAN-538 PLAN-379 tweaks to report page
    - make it so staff cannot click on the report they may not run
    - start description process
    - make table of contents at top of reports page
    - adjust styling of titles

--- app/javascript/reports/reports_screen.vue
+++ app/javascript/reports/reports_screen.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="mb-3 scrollable">
-    <p>These are interim reports.</p>
-    <p><b>Participants</b></p>
+    <p class="mt-2">These are interim reports.</p>
+    <ul>
+      <li><a href="#participants">Participants</a></li>
+      <li><a href="#sessions">Sessions</a></li>
+      <li><a href="#conflicts">Conflicts</a></li>
+      <li><a href="#schedule">Schedule</a></li>
+    </ul>
+    <h5><a id="participants"></a>Participants</h5>
     <ul>
       <li>
-        <a href="/report/participant_selections" target="_blank">Participant Selections</a>
+        <a href="/report/participant_selections" target="_blank">Participant Selections</a><br />
+        <p class="ml-2">
+          <strong><em>Description</em></strong>: Sessions and rankings selected by people, one line per person and session<br />
+          <strong><em>Fields</em></strong>: Person name, published name, participant status, session title, ranking, ranking notes, area(s) for the session, has person been assigned (y/n), has session been scheduled (y/n)<br />
+          <strong><em>Session data included</em></strong>: any session selected by at least one person<br />
+          <strong><em>Person data included</em></strong>: participant status of applied, probable, vetted, invite_pending, invited, accepted
+        </p>
       </li>
       <li>
         <a href="/report/participant_availabilities" target="_blank">Participant Availabilities</a>
+        <p class="ml-2">
+          <strong><em>Description</em></strong>: Information in the person's &quot;Availability &amp; Interests&quot; tab (daily limit and con limit, days/times available, special events to attend)<br />
+          <strong><em>Fields</em></strong>: Person name, published name, participant status, attendance type (in-person, virtual, hybrid), availabilities (days/times), limits (daily &amp; con/&quot;global&quot;), exclusions (special events to attend), availability notes<br />
+          <strong><em>Person data included</em></strong>: participant status of applied, probable, vetted, invite_pending, invited, accepted
+        </p>
       </li>
       <li>
         <a href="/report/assigned_sessions_by_participant" target="_blank">Assigned Sessions by Participant</a>
@@ -16,7 +33,8 @@
         <a href="/report/people_and_submissions" target="_blank">People and Survey Submissions</a>
       </li>
       <li>
-        <a href="/report/participant_do_not_assign_with" target="_blank">Participants and Do Not Assign With</a>
+        <a href="/report/participant_do_not_assign_with" target="_blank" v-if="currentUserIsAdmin">Participants and Do Not Assign With</a>
+        <span v-if="currentUserIsStaff" class="text-muted font-italic" title="You do not have the right set of permissions to run this report." v-b-tooltip>Participants and Do Not Assign With</span>
       </li>
       <li>
         <a href="/report/session_reports/participants_over_session_limits" target="_blank">Participants over Daily Limits</a>
@@ -33,7 +51,7 @@
       </li>
     </ul>
 
-    <p><b>Sessions</b></p>
+    <h5><a id="sessions"></a>Sessions</h5>
     <ul>
       <li>
         <a href="/report/session_selections" target="_blank">Session Selections</a>
@@ -61,7 +79,7 @@
       </li>      
     </ul>
 
-    <p><b>Conflicts</b></p>
+    <h5><a id="conflicts"></a>Conflicts</h5>
     <ul>
       <li>
         <a href="/report/conflict_reports/people_outside_availability" target="_blank">People outside of Availability</a>
@@ -89,7 +107,7 @@
       </li>
     </ul>
 
-    <p><b>Schedule</b></p>
+    <h5><a id="schedule"></a>Schedule</h5>
     <ul>
       <li>
         <a href="/report/schedule_by_room_then_time" target="_blank">Schedule by Room then Time</a>
@@ -109,8 +127,13 @@
 </template>
 
 <script>
+import personSessionMixin from '@/auth/person_session.mixin'
+
 export default {
-  name: "ReportsScreen"
+  name: "ReportsScreen",
+  mixins: [
+    personSessionMixin
+  ]
 }
 </script>
 
