d3735f61d5d75d6ae6f49f0795f9a627cc98debb
Henry <henry.balen@chicon.org>
2022-07-28 12:53:22 -0400

    WIP

--- app/controllers/mail_histories_controller.rb
+++ app/controllers/mail_histories_controller.rb
@@ -1,3 +1,37 @@
 class MailHistoriesController < ResourceController
-# TBD
+  MODEL_CLASS = 'MailHistory'.freeze
+  # TODO: the classes below
+  SERIALIZER_CLASS = 'MailHistorySerializer'.freeze
+  POLICY_CLASS = 'MailHistoryPolicy'.freeze
+  POLICY_SCOPE_CLASS = 'MailHistoryPolicy::Scope'.freeze
+
+  def belongs_to_param_id
+    params[:person_id]
+  end
+
+  def belong_to_class
+    Person
+  end
+
+  def belongs_to_relationship
+    'mail_histories'
+  end
+
+  def paginate
+    false
+  end
+
+  # Should be read only
+  # def allowed_params
+  #   %i[
+  #     id
+  #     lock_version
+  #     person_id
+  #     mailing_id
+  #     email_status
+  #     date_sent
+  #     content
+  #     subject
+  #   ]
+  # end
 end
