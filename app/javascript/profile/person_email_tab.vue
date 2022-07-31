504a221c17d1b5013c9ec5e57a830838ec7c3a8a
Gail Terman <gterman@gmail.com>
2022-07-31 18:51:59 -0400

    PLAN-450 lightweight email tab.

---
+++ app/javascript/profile/person_email_tab.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div v-for="mail in fetchedMailings" :key="mail.id" class="mb-2">
+          <h5> {{DateTime.fromISO(mail.date_sent).toFormat("DDDD, t ZZZZ")}} </h5>
+          <dl>
+            <dt class="font-weight-bold">Subject</dt>
+            <dd class="ml-2">{{mail.subject}}</dd>
+            <dt class="font-weight-bold">Body</dt>
+            <dd class="ml-2" v-html="mail.content"></dd>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon';
+import { mapActions } from 'vuex';
+import { personModel as model } from '@/store/person.store';
+import { modelMixinNoProp } from '@/mixins';
+
+export default {
+  name: 'PersonEmailTab',
+  data: () => ({
+    fetchedMailings: [],
+    DateTime,
+    model,
+  }),
+  mixins: [
+    modelMixinNoProp
+  ],
+  methods: {
+    ...mapActions({
+      get: 'jv/get'
+    }),
+    fetchMailings() {
+      this.get(`/person/${this.selected.id}/mail_histories`).then((data) => {
+          let {_jv, ...filteredMailings} = data;
+          let sortableMailings = Object.values(filteredMailings);
+          sortableMailings.sort((a, b) => DateTime.fromISO(b.date_sent) - DateTime.fromISO(a.date_sent));
+          this.fetchedMailings = sortableMailings
+      });
+    }
+  },
+  mounted() {
+    if (this.selected) {
+      this.fetchMailings();
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
