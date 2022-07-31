3a040fc2d8027e0c24e7efa13913e6d6b17745c4
Gail Terman <gterman@gmail.com>
2022-07-31 15:08:40 -0400

    PLAN-617 fix some pagination issues
    - add a frontend-persistent page size across all tables
    - add a go-to-page box
    - add a spinner while the table loads

--- app/javascript/store/app.store.js
+++ app/javascript/store/app.store.js
@@ -1,14 +1,19 @@
 export const MAGICAL_RELOAD = 'MAGICAL RELOAD'
+export const SET_PER_PAGE = 'SET PER PAGE';
 
 export const appStore = {
   state: {
     magicalReload: 0,
     reloadedAt: new Date(),
+    perPage: 20
   },
   mutations: {
     [MAGICAL_RELOAD] (state) {
       state.magicalReload += 1;
       state.reloadedAt = new Date();
+    },
+    [SET_PER_PAGE] (state, amt) {
+      state.perPage = amt;
     }
   }
 }
