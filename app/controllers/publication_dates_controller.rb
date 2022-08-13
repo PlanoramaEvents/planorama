af2598de8a0176253febd9f6a51fa349ba57effd
Henry <henry.balen@chicon.org>
2022-08-13 07:17:58 -0400

    fix condition and ensure nothing is done during a reset

--- app/controllers/publication_dates_controller.rb
+++ app/controllers/publication_dates_controller.rb
@@ -6,12 +6,19 @@ class PublicationDatesController < ResourceController
   DEFAULT_ORDER = 'desc'.freeze
 
   def reset
+    # Make sure that people can not publish etc while we clean up
+    PublicationStatus.last.update(status: 'inprogress')
+
+    # Get rid of published data first
     PublishedSession.destroy_all
+
+    # Then get rid of the audit
+    Audit::PublishedSessionVersion.delete_all
+
+    # Then remove caches, dates, and status
     PublishSnapshot.delete_all
     PublicationDate.delete_all
     PublicationStatus.delete_all
-
-    Audit::PublishedSessionVersion.delete_all
 
     render status: :ok,
           json: { message: 'publication reset' }.to_json,
