240d993f40223994ec5eaedd5758e671c4544c14
Gail Terman <gterman@gmail.com>
2022-10-15 18:43:14 -0400

    PLAN-791 add exclusions and other considerations to flyout

--- app/javascript/profile/availability_flyout_tab.vue
+++ app/javascript/profile/availability_flyout_tab.vue
@@ -19,7 +19,7 @@
       <div class="flex-grow-1 flex-shrink-1 flex-basis-50">
         <dl>
           <dt>Maximum number of program items per day</dt>
-          <dd class="ml-2 font-italic">
+          <dd class="ml-2 font-italic mb-4">
             <table>
               <tr v-for="{text, value} of dayOptions" :key="`${text}-${value}`">
                 <td>{{text}}&nbsp;</td>
@@ -27,15 +27,19 @@
               </tr>
             </table>
           </dd>
+          <dt>Do not schedule against these specific events</dt>
+          <b-overlay :show="exclusionsLoading" spinner-variant="primary" style="min-height: 5rem;" class="mb-4">
+            <dd class="ml-2 font-italic" v-for="exclusion in exclusions" :key="exclusion.id">
+              {{exclusionsMap[exclusion.exclusion_id]}}
+            </dd>
+            <dd class="ml-2 font-italic text-muted" v-if="!exclusions.length">None Selected</dd>
+          </b-overlay>
+          <dt>Other scheduling considerations</dt>
+          <dd class="ml-2 font-italic keep-format">{{selected.availability_notes}}</dd>
+          <dd class="ml-2 font-italic text-muted" v-if="!selected.availability_notes">Not Specified</dd>
         </dl>
       </div>
     </div>
-    <!-- <div>
-      <dl>
-        <dt>Exclusions FIX ME</dt>
-        <dd>{{JSON.stringify(exclusions, undefined, 2)}}</dd>
-      </dl>
-    </div> -->
   </div> 
 </template>
 
@@ -46,12 +50,14 @@ import { personModel as model} from '@/store/person.store';
 import conventionTimezoneMixin from '@/shared/convention-timezone.mixin';
 import { GET_AVAILABILITY } from '@/store/availability.store';
 import { GET_PERSON_EXCLUSIONS } from '@/store/person_exclusion.store';
+import { settingsMixin } from '@/mixins';
 
 export default {
   name: "AvailabilityFlyoutTab",
   mixins: [
     modelMixinNoProp,
     conventionTimezoneMixin,
+    settingsMixin,
   ],
   data: () => ({
     model,
@@ -72,6 +78,11 @@ export default {
           return availabilities.map(a => `${a.start.setZone(this.conventionTimezone).toLocaleString({weekday: 'long', hour: 'numeric', minute: '2-digit'})} - ${a.end.setZone(this.conventionTimezone).toLocaleString({hour: 'numeric', minute: '2-digit', timeZoneName: 'short'})}`)
       }
       return [];
+    },
+    exclusions() {
+      if (this.selected) {
+        return this.exclusionsForPerson(this.selected);
+      }
     },
     dayOptions() {
       return this.daysArray.map(d => ({text: d.toFormat('EEEE'), value: d.toFormat('y-MM-dd')}))
@@ -114,6 +125,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
+dd {
+  margin-bottom: 0px;
+}
 </style>
