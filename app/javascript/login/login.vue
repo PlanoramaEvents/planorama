30ed724148b158f0164f4d3466ac3f0449ee24bd
Henry <henry.balen@chicon.org>
2023-03-07 08:16:07 -0500

    todo

--- app/javascript/login/login.vue
+++ app/javascript/login/login.vue
@@ -26,6 +26,7 @@
     <!--iea-modal @cancel="onIeaCancel" @ok="onIeaAgree"></iea-modal-->
     <div class="d-flex flex-column mb-3">
       <router-link to="/login/forgot">Never set up your password? Set it up now</router-link>
+      <!-- TODO: need to pass the URL for redirect to the login new somehow -->
       <router-link to="/login/new">Create account</router-link>
     </div>
   </div>
