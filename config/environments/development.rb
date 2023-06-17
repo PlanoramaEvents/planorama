e42cb2f66ba2fe2c4eca8d6fcbf8863762fbbd9d
Gail Terman <gterman@gmail.com>
2023-06-17 13:56:25 -0400

    add planorama2 as an allowed host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << "planorama.loophole.site"
+  config.hosts << "planorama.loophole.site" << "planorama2.loophole.site"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
