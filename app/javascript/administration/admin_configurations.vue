9211158c1c40e7a13e3f77adaa45d4893a92ef0f
Gail Terman <gterman@gmail.com>
2023-10-08 19:17:35 -0400

    PLAN-873 move editor to new admin page

---
+++ app/javascript/administration/admin_configurations.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="scrollable">
+  <h1>Admin - Configuration</h1>
+  <b-tabs content-class="mt-3 container-fluid">
+    <b-tab title="Page Content Editor">
+      <page-content-editor></page-content-editor>
+    </b-tab>
+  </b-tabs>
+</div> 
+</template>
+
+<script>
+import PageContentEditor from '@/page-content/page_content_editor.vue';
+
+export default {
+  name: "AdminConfigurations",
+  components: {
+    PageContentEditor
+  }
+}
+</script>
+
+<style>
+
+</style>
