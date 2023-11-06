b47a2885fe879337a88f2463cb2c115284803eac
Henry <henry.balen@gmail.com>
2023-11-06 09:17:22 -0500

    Add clear button for clearing custom content
    NOTE: primary and secondary buttons are sane color which
    probably should be changed (custom.css)

--- app/javascript/page-content/page_content.mixin.js
+++ app/javascript/page-content/page_content.mixin.js
@@ -1,8 +1,9 @@
 import { mapGetters, mapState, mapActions } from 'vuex';
 import toastMixin from '../shared/toast-mixin';
 import { NEW_PAGE_CONTENT } from '@/store/page_content.store';
-import { PAGE_CONTENT_SAVE_SUCCESS, PAGE_CONTENT_SAVE_ERROR } from '../constants/strings'
+import { PAGE_CONTENT_SAVE_SUCCESS, PAGE_CONTENT_SAVE_ERROR, PAGE_CONTENT_SUCCESS_DELETE, PAGE_CONTENT_ERROR_DELETE } from '../constants/strings'
 import modelUtilsMixin from '../store/model_utils.mixin'
+import { DELETE } from "@/store/model.store"
 
 export const pageContentMixin = {
   mixins: [toastMixin, modelUtilsMixin],
@@ -10,6 +11,7 @@ export const pageContentMixin = {
     ...mapActions({
       new_page_content: NEW_PAGE_CONTENT
     }),
+    // Do we need a delete ?
     savePageContent(newPageContent, success_text = PAGE_CONTENT_SAVE_SUCCESS, error_text = PAGE_CONTENT_SAVE_ERROR) {
       if (newPageContent.id) {
         return this.save_model('page_content', newPageContent)
@@ -20,6 +22,9 @@ export const pageContentMixin = {
         )
       }
     },
+    deletePageContent(itemOrId, success_text = PAGE_CONTENT_SUCCESS_DELETE, error_text = PAGE_CONTENT_ERROR_DELETE) {
+      return this.toastPromise(this.$store.dispatch(DELETE, { model: 'page_content', itemOrId: itemOrId }), success_text, error_text);
+    }
   }
 }
 
