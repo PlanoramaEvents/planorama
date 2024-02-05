112f87d14f933ecd7ae9569d471a58fe9adcde26
Henry <henry.balen@gmail.com>
2024-02-05 16:35:08 -0500

    Change the text to be generic message instead of the date in 2022

--- app/javascript/profile/person_details.vue
+++ app/javascript/profile/person_details.vue
@@ -62,7 +62,7 @@
             <a target="blank" href="https://www.timeanddate.com/worldclock/meeting.html">
               https://www.timeanddate.com/worldclock/meeting.html
             </a>
-            and check by specifying the date of September 1 2022 and your location as well as UTC/GMT
+            and check by specifying the date of first day of the convention and your location as well as UTC/GMT
           </small>
         </b-form-group>
       </div>
