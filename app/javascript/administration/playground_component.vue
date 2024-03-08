6779cb6704365238664fb2c3fb3077f1b0cddb94
Gail Terman <gterman@gmail.com>
2024-03-07 21:34:44 -0500

    PLAN-879 stepper attempt 1 - partial

--- app/javascript/administration/playground_component.vue
+++ app/javascript/administration/playground_component.vue
@@ -1,6 +1,10 @@
 <template>
 <div class="container-fluid scrollable">
   <h1>Preview features</h1>
+  <h2>Stepper</h2>
+  <div class="w-75">
+    <stepper></stepper>
+  </div>
   <h2>TwoSidedMultiSelect</h2>
   <div class="w-75">
   <two-sided-multi-select v-model="multiSelectValue" :options="multiSelectOptions"></two-sided-multi-select>
@@ -13,11 +17,13 @@
 
 <script>
 import TwoSidedMultiSelect from "@/components/two_sided_multi_select.vue";
+import Stepper from "@/components/stepper.vue"
 
 export default {
   name: "PlayGroundComponent",
   components: {
-    TwoSidedMultiSelect
+    TwoSidedMultiSelect,
+    Stepper
   },
   data: () => ({
     multiSelectOptions: ["one", "two", "three", "four", "five", "six"],
---
+++ app/javascript/components/stepper.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="stepper">
+    <div class="shishkabob">
+      <div class="buttons">
+        <button class="btn btn-secondary py-4 px-5 selected">Before Draft Publish</button>
+        <button class="btn btn-secondary py-4 px-5">After Draft Publish</button>
+      </div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stepper"
+}
+</script>
+
+<style lang="sass" scoped>
+.stepper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.content {
+  border: 2px solid red;
+  width: 100%;
+  height: 200px;
+}
+
+.shishkabob {
+  width: 62.5%;
+  margin-bottom: 1rem;
+  .buttons {
+    display: flex;
+    justify-content: space-around;
+
+    .btn-secondary {
+      border-color: #ccb5f0;
+      background: #ccb5f0;
+      color: black;
+
+      &:hover {
+        border-color: #53269f;
+      }
+      
+      &.selected {
+        border-color: #53269f;
+        background: #53269f;
+        color: white;
+      }
+    }
+  }
+
+  &:before {
+    content: '';
+    border: 4px solid #ccb5f0;
+    height: 0;
+    width: 100%;
+    text-align: center;
+    position: relative;
+    top: 38px;
+    display: block;
+    z-index: -1;
+  }
+
+}
+
+</style>
