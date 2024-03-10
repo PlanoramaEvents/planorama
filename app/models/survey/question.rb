9ff087d8392dcdfa049b7b53aa3413ee02974557
Henry <henry.balen@gmail.com>
2024-03-10 17:37:15 -0400

    Fix for sorting of questions in xls file

--- app/models/survey/question.rb
+++ app/models/survey/question.rb
@@ -35,10 +35,9 @@ class Survey::Question < ApplicationRecord
   # Scopes to deal with the soft deletes
   scope :not_deleted, -> { where(deleted_at: nil) }
   scope :deleted, -> { where('survey_questions.deleted_at is not null') }
+  scope :sorted {includes(:page).order(['survey_pages.sort_order asc', 'survey_questions.sort_order asc'])}
 
   has_paper_trail versions: { class_name: 'Audit::SurveyVersion' }, ignore: [:updated_at, :created_at, :lock_version, :sort_order]
-
-  # default_scope {includes(:page).order(['survey_pages.sort_order asc', 'survey_questions.sort_order asc'])}
 
   belongs_to :page,
              class_name: 'Survey::Page',
