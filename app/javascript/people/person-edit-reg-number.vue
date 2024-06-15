416c35f8c5da66676e4cb433ff759754721b1b0e
Gail Terman <gterman@gmail.com>
2024-06-15 16:33:12 -0400

    replace person and confirmation dialog

--- app/javascript/people/person-edit-reg-number.vue
+++ app/javascript/people/person-edit-reg-number.vue
@@ -3,9 +3,10 @@
     <edit-modal
       id="edit-reg-number"
       title="Edit Ticket Number"
-      @hidden="onHide()"
-      @ok="manualMatchSelected()"
-      :ok-disabled="!selected"
+      @close="onHide()"
+      @cancel="onHide()"
+      @ok.prevent="attemptLink()"
+      :ok-disabled="!selected || (matchedPerson && !viewed)"
     >
       <form @submit.prevent="searchRegId()">
         <label for="reg-id-input">Ticket Number</label>
@@ -23,13 +24,27 @@
         </div>
       </form>
       <div class="display-sync-data">
-        <h6 v-if="selected">{{ REG_ID_FOUND }}<span 
-          v-if="matchedPerson">, already linked to <strong>{{ matchedPerson }}</strong> in Planorama.</span><span
-           v-if="!matchedPerson">, not currently linked to anybody in Planorama.</span>
+        <h6 v-if="selected">
+          {{ REG_ID_FOUND
+          }}<span v-if="matchedPerson"
+            >, already linked to <strong>{{ matchedPerson }}</strong> in
+            Planorama.</span
+          ><span v-if="!matchedPerson"
+            >, not currently linked to anybody in Planorama.</span
+          >
         </h6>
-        <b-button v-if="matchedPerson" variant="link" v-b-toggle.sync-data class="pl-0">See matched person</b-button>
-        <b-collapse id="sync-data" :visible="!matchedPerson">
-          <display-sync-data :regData="selected" class="ml-2"></display-sync-data>
+        <b-button
+          v-if="matchedPerson"
+          variant="link"
+          v-b-toggle.sync-data
+          class="pl-0"
+          >See matched person</b-button
+        >
+        <b-collapse id="sync-data" :visible="!matchedPerson" @shown="markViewed()">
+          <display-sync-data
+            :regData="selected"
+            class="ml-2"
+          ></display-sync-data>
         </b-collapse>
         <div class="text-muted font-italic" v-if="!selected && !searched">
           {{ REG_ID_SEARCH_PLACEHOLDER }}
@@ -39,6 +54,12 @@
         </div>
       </div>
     </edit-modal>
+    <plano-modal id="unmatch-match">This will unlink {{ matchedPerson }} from ticket number {{ selectedRegNumber }}, and link to the current profile.
+      <template #modal-footer="{ok, cancel}">
+        <b-button variant="link" @click="cancel()">Cancel</b-button>
+        <spinner-button variant="danger" :loading="unlinkLinkLoading" @click="unlinkLink(ok)">Ok</spinner-button>
+      </template>
+    </plano-modal>
   </div>
 </template>
 
@@ -46,6 +67,7 @@
 import DisplaySyncData from "@/registrations/display_sync_data.vue";
 import EditModal from "@/components/edit_modal.vue";
 import PlanoModal from "@/components/plano_modal.vue";
+import SpinnerButton from "@/shared/spinner-button.vue";
 import { mapActions } from "vuex";
 import {
   GET_REG_BY_ID,
@@ -58,6 +80,7 @@ import {
   REG_ID_FOUND,
   REG_ID_NOT_FOUND,
 } from "@/constants/strings";
+import { UNLINK_PERSON } from "@/store/person.store";
 
 export default {
   name: "PersonEditRegNumber",
@@ -65,6 +88,7 @@ export default {
     DisplaySyncData,
     EditModal,
     PlanoModal,
+    SpinnerButton,
   },
   mixins: [modelMixinNoProp, personSyncDatumMixin],
   props: {
@@ -75,6 +99,8 @@ export default {
   data: () => ({
     partialRegId: null,
     searched: false,
+    viewed: false,
+    unlinkLinkLoading: false,
     model,
     REG_ID_SEARCH_PLACEHOLDER,
     REG_ID_FOUND,
@@ -86,17 +112,52 @@ export default {
     },
     matchedPerson() {
       return this.selected?.matched_person?.published_name;
+    },
+    selectedRegNumber() {
+      return this.selected?.registration_number;
     }
   },
   methods: {
     ...mapActions({
       getRegById: GET_REG_BY_ID,
+      unlinkPerson: UNLINK_PERSON,
     }),
     onHide() {
       this.unselect();
       this.partialRegId = null;
       this.searched = false;
+      this.viewed = false;
     },
+    markViewed() {
+      this.viewed = true;
+    },
+    attemptLink() {
+      if (this.matchedPerson) {
+        this.$bvModal.show("unmatch-match");
+      } else {
+        this.manualMatchSelected().then(() => {
+          this.$bvModal.hide("edit-reg-number");
+          this.onHide();
+        })
+      }
+    },
+    unlinkLink(okMethod) {
+      // first unlink person
+      console.log("should unlink", this.selected?.matched_person);
+      this.unlinkLinkLoading = true;
+      this.unlinkPerson({
+        person: this.selected.matched_person,
+        select: false,
+      }).then(() => {
+        this.manualMatchSelected().then(() => {
+          this.unlinkLinkLoading = false;
+          okMethod();
+          this.$bvModal.hide("edit-reg-number");
+          this.onHide();
+        });
+      });
+    },
+
     searchRegId() {
       this.getRegById({ id: this.regId }).then(() => {
         this.searched = true;
