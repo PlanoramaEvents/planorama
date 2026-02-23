ba13e9a8fa379bfbdb992362bf174725eef3a719
Henry <henry.balen@gmail.com>
2026-02-23 10:51:18 -0500

    Add new model dialogs for venue and room

--- app/javascript/venues/venues_table.vue
+++ app/javascript/venues/venues_table.vue
@@ -1,5 +1,11 @@
 <template>
+  <plano-modal id="add-venue-modal" title="Add Venue" @ok="onNew">
+    <b-form-group label="Venue Name">
+      <b-form-input id="venue-new-venue-name" type="text" v-model="newVenueName"></b-form-input>
+    </b-form-group>
+  </plano-modal>
   <table-vue
+    @new="openNewModal"
     :model="venue"
     :columns="columns"
     :show-add="true"
@@ -16,6 +22,7 @@
 
 <script>
 import TableVue from '@/components/table_vue';
+import PlanoModal from '@/components/plano_modal.vue';
 import TooltipOverflow from '@/components/tooltip-overflow';
 import { venue_columns as columns } from './venue';
 import { mapActions } from 'vuex';
@@ -25,16 +32,27 @@ export default {
   name: 'VenuesTable',
   components: {
     TableVue,
-    TooltipOverflow
+    TooltipOverflow,
+    PlanoModal
   },
   data() {
     return {
       columns,
-      venue
+      venue,
+      newVenueName: null
     }
   },
   methods: {
     ...mapActions({newVenue: NEW_VENUE}),
+    openNewModal() {
+      this.newVenueName = null;
+      this.$bvModal.show('add-venue-modal');
+    },
+    onNew() {
+      this.newVenue({name: this.newVenueName}).then((data) => {
+        this.$router.push(`/venues/edit/venue/${data.id}`)
+      })
+    },
     init() {
       this.$refs['venues-table'].fetchPaged()
     }
