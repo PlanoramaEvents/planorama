b630635bf14eb301fd34b9e68d7d0c5945ec2a15
Gail Terman <gterman@gmail.com>
2026-05-29 02:16:48 -0400

    PLAN-1115 move survey reports to bottom of existing reports page

--- app/javascript/reports/reports_tabs.vue
+++ app/javascript/reports/reports_tabs.vue
@@ -7,34 +7,20 @@
       <b-tab title="Area Label Report" :active="tab === 'area-label-report'" lazy>
         <labels-by-area></labels-by-area>
       </b-tab>
-      <b-tab title="Survey Reports" :active="tab === 'survey-reports'" lazy v-if="collection.length">
-        <survey-reports :report-configs="collection"></survey-reports>
-      </b-tab>
     </b-tabs>
   </div>
 </template>
 
 
 <script>
-import SurveyReports from "@/reports/survey_reports.vue"
 import Reports from "@/reports/reports.vue"
-import { modelMixinNoProp } from '@/store/model.mixin';
-import { reportConfigModel } from '@/store/survey/report_config.store'
 import LabelsByArea from '@/reports/labels-by-area.vue';
 
 export default {
   name: "ReportsTabs",
-  components: {Reports, SurveyReports, LabelsByArea},
+  components: {Reports, LabelsByArea},
   props: [
     'tab'
-  ],
-  data() {
-    return {
-      model: reportConfigModel
-    }
-  },
-  mixins : [
-    modelMixinNoProp
   ],
   methods: {
     handleTabActivation(newTab, oldTab, bvEvent) {
@@ -57,9 +43,6 @@ export default {
         }
       })
     }
-  },
-  mounted() {
-    this.fetch()
   },
 }
 </script>
